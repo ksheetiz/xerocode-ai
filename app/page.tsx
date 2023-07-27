998abf6e4e1c3174d72b4ee4f47003e9d0298d1f
ksheetiz agrahari <ksheetiz43@gmail.com>
2023-07-28 01:44:53 +0530

    Added Animations

--- app/page.tsx
+++ app/page.tsx
@@ -7,98 +7,86 @@ import { Heading } from './components/Heading'
 import { Input } from './components/Input'
 import ItemsCard from './components/Cards/ItemsCard'
 import FeatureCard from './components/Cards/FeatureCard'
-import { useEffect } from 'react'
+
 import FeatureCard1 from './components/Cards/FeatureCard1'
-
-
+import TrackVisibility from 'react-on-screen';
+import 'animate.css';
 
 export default function Home() {
-
-  // const observer = new IntersectionObserver((entries)=>{
-  //   //console.log("The observer just ran");
-  //   entries.forEach((entry) => {
-  //     if(entry.isIntersecting){
-  //       entry.target.classList.add('show');
-  //     }
-  //     //? Uncomment to run animation multiple times
-  //     // else{
-  //     //   entry.target.classList.remove('show');
-  //     // }
-  //   })
-  // })
-
-  // const hiddenElements = document.querySelectorAll('.show');
-
-  // const dummyFunction = async() => {
-  //   console.log("The dummy func ran !");
-  //   hiddenElements.forEach((el) => observer.observe(el));
-  // }
-
-  // useEffect(() => {
-  //   dummyFunction();
-  // }, [dummyFunction])
 
   return(
     <ClientOnly>
       <Container>
-
+      
             {/* TOP SECTION */}
 
-        <div className='flex flex-col'>
-            <div className='show'>
+          <div className={`flex flex-col `}>
+            <div className=''>
               <Heading title='Integrate AI Throughtout Your Workflow' combination="bg-gradient-to-r from-fuchsia-500 via-fuchsia-100 to-indigo-600" gradient center/>
             </div>
-            <div className='mt-10 show'>
+            <div className='mt-10 '>
               <Heading title='Code Doctor' center/>
             </div>
-            <div className='mt-10 show'>
+            <div className='mt-10 '>
               <Heading subtitle='Make process where AI collaborates with your team throughtout' center/>
               <Heading subtitle='the whole developement process.' center/>
             </div>
-            <div className='mb-10 mt-5 show'>
+            <div className='mb-10 mt-5 '>
               <Input/>
             </div>
-            <div className='flex justify-center items-center mt-10 relative show'>
+            <TrackVisibility>
+
+            {({isVisible}) => 
+            <div className={`flex justify-center items-center mt-10 relative ${isVisible ? "animate__animated animate__zoomIn" : ""}`}>
               <Image src="/images/robo-back.png" alt='background' className='absolute top-0' width="1200" height="1200" />
               <Image src="/images/robot.png" alt='Robot' width="1300" height="1300"/>
-            </div>
+            T</div>}
 
+            </TrackVisibility>
             {/* MID SECTION */}
 
-            <div className='mt-28 show'>
-              <Heading title='Boost the speed of your development and test' combination='bg-gradient-to-r from-indigo-600 to-fuchsia-500' gradient center/>
-              <Heading title='cycles.' gradient center combination='bg-gradient-to-r from-indigo-600 to-fuchsia-500' />
-              <Heading subtitle='With AI-driven test case generation and code analysis, problems may be found and fixed' center/>
-              <Heading subtitle='more quickly.' center/>
-              <div className='flex flex-row justify-start mt-16'>
-                <ItemsCard item1='CI/CD Pipeline Generate' item2='Build / Deployment' width={96}/>
-              </div>
-              <div className='flex flex-row items-center h-auto justify-center mt-20 mb-40 xl:mt-20 xl:mb-20'>
-                <div className='neon-circle xl:fixed relative'>
-                </div>
-                <div className=' absolute w-auto h-auto'>
-                  <div className=' text-white text-center'>
-                    <p className='font-thin text-5xl pb-3 font-sans'>CODE</p>
-                    <p className='font-light text-5xl font-sans'>DOCTOR 👋</p>
-                  </div>
-                  <div className='hidden text-white text-center text-5xl'>
-                    <p>WHAT CAN I</p>
-                    <p>HELP</p>
-                    <p>YOU WITH ?</p>
-                  </div> 
-                </div>
-              </div>
-              <div className='flex flex-row justify-end mt-12 mb-24'>
-                <ItemsCard item1='Generate Test Cases' item2='Code Analysis' width={96} />
-              </div>
-            </div>
+            <TrackVisibility>
 
+            {({isVisible}) => 
+              <div className={`mt-28 ${isVisible ? "animate__animated animate__fadeIn" : ""}`}>
+                <Heading title='Boost the speed of your development and test' combination='bg-gradient-to-r from-indigo-600 to-fuchsia-500' gradient center/>
+                <Heading title='cycles.' gradient center combination='bg-gradient-to-r from-indigo-600 to-fuchsia-500' />
+                <Heading subtitle='With AI-driven test case generation and code analysis, problems may be found and fixed' center/>
+                <Heading subtitle='more quickly.' center/>
+                <div className='flex flex-row justify-start mt-16'>
+                  <ItemsCard item1='CI/CD Pipeline Generate' item2='Build / Deployment' width={96}/>
+                </div>
+                <div className='flex flex-row items-center h-auto justify-center mt-20 mb-20 xl:mt-0 xl:mb-0'>
+                  <div className='neon-circle relative'>
+                  </div>
+                  <div className='absolute w-auto h-auto'>
+                    <div className=' text-white text-center'>
+                      <p className='font-thin text-5xl pb-3 font-sans'>CODE</p>
+                      <p className='font-light text-5xl font-sans'>DOCTOR 👋</p>
+                    </div>
+                    <div className='hidden text-white text-center text-5xl'>
+                      <p>WHAT CAN I</p>
+                      <p>HELP</p>
+                      <p>YOU WITH ?</p>
+                    </div> 
+                  </div>
+                </div>
+                <div className='flex flex-row justify-end mt-12 mb-24'>
+                  <ItemsCard item1='Generate Test Cases' item2='Code Analysis' width={96} />
+                </div>
+              </div>}
+            </TrackVisibility>
             {/* LOWER SECTION */}
 
-            <div className='mt-14 show'>
+
+            <div className='mt-14 '>
               <Heading title='Use AI to handle workflows in your chat platforms.' combination='bg-gradient-to-r from-indigo-600 to-fuchsia-500' gradient center/>
               
-              <div className='flex flex-col md:flex-row justify-between items-center'>
+              <TrackVisibility>
+
+            {({isVisible}) => 
+               <>   
+              <div className={`flex flex-col md:flex-row justify-between items-center ${isVisible ? "animate__animated animate__zoomIn" : ""}`}>
                 <FeatureCard title="ask questions" body="To assist with managing the development, testing, and 
                   deployment process, ask it questions or issue instructions." label="COMING SOON !"/>
                 <FeatureCard title="execute" body="With a single command, you may run a series of tests or install a 
@@ -109,14 +97,16 @@ export default function Home() {
                 granting developers the freedom they require to protect their work frequently and early"/>
 
               </div>
+              </>}
+              </TrackVisibility>
 
             </div>
 
-            <div className='mt-24 show'>
+            <div className='mt-24 '>
               <Heading title='Automate all aspects of your delivery process with' combination="bg-gradient-to-r from-fuchsia-500 via-fuchsia-100 to-indigo-600" gradient center/>
               <Heading title='AI support.' combination="bg-gradient-to-r from-fuchsia-500 via-fuchsia-100 to-indigo-600" gradient center/>
             </div>
-            <div className='mt-12 show'>
+            <div className='mt-12 '>
               <Heading subtitle='Get Early Access' center/>
               <Input/>
             </div>
@@ -125,7 +115,7 @@ export default function Home() {
 
       {/* FOOTER */}
 
-        <div className='mt-16 -mb-16 show'>
+        <div className='mt-16 -mb-16 '>
             <p className='text-center text-white'>Copyright © 2023 <span className='font-bold'> EXOCODE TECHNOLOGIES </span> | ALL rights reserved</p>
         </div>
     </ClientOnly>
